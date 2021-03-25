d45693d522151ab96a4de07d451cd292d9715a72
Laina Sanders <lainarose2017@gmail.com>
2021-03-25 09:49:21 -0700

    Completed denormalized insertion

--- load_tweets.sh
+++ load_tweets.sh
@@ -4,8 +4,10 @@ test-data.zip
 
 for file in $files; do
     # call the load_tweets.py file to load data into pg_normalized
+    :
 done
 
 for file in $files; do
     # use SQL's COPY command to load data into pg_denormalized
+    unzip -p $file | sed 's/\\u0000//g' | psql postgres://postgres:pass@localhost:1127/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
